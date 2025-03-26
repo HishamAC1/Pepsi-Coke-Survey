4ce029fe8dfcb5018021f7afb9e8ad8cfa6b2d53
HishamAC1 <hishamac613@yahoo.com>
2025-03-26 06:44:49 -0500

    1. Added instance of Survey to Main 2. Added functionality to survey multiple users

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
+// src/Main.java
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Survey.main(args);
     }
 }
--- src/Survey.java
+++ src/Survey.java
@@ -1,32 +1,46 @@
+// src/Survey.java
 import java.util.Scanner;
 
 public class Survey {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int cokeCount = 0;
-        int pepsiCount = 0;
+        System.out.print("Enter the number of users: ");
+        int numberOfUsers = scanner.nextInt();
+        scanner.nextLine(); // Consume newline
 
-        System.out.println("Please select your preference for each pair of products:");
+        int[] cokeCounts = new int[numberOfUsers];
+        int[] pepsiCounts = new int[numberOfUsers];
 
-        cokeCount += getUserChoice(scanner, "Coke", "Pepsi");
-        cokeCount += getUserChoice(scanner, "Sprite ", "7-Up");
-        cokeCount += getUserChoice(scanner, "Fanta", "Crush");
-        cokeCount += getUserChoice(scanner, "Minute Maid", "Tropicana");
-        cokeCount += getUserChoice(scanner, "Barq's Root Beer", "MUG Root Beer");
-        cokeCount += getUserChoice(scanner, "Dasani", "Aquafina ");
+        for (int i = 0; i < numberOfUsers; i++) {
+            System.out.println("User " + (i + 1) + ": Please select your preference for each pair of products:");
 
-        pepsiCount = 6 - cokeCount; // Since there are 6 questions, the remaining choices are for Pepsi
+            int cokeCount = 0;
+            cokeCount += getUserChoice(scanner, "Coke", "Pepsi");
+            cokeCount += getUserChoice(scanner, "Sprite", "7-Up");
+            cokeCount += getUserChoice(scanner, "Fanta", "Crush");
+            cokeCount += getUserChoice(scanner, "Minute Maid", "Tropicana");
+            cokeCount += getUserChoice(scanner, "Barq's Root Beer", "MUG Root Beer");
+            cokeCount += getUserChoice(scanner, "Dasani", "Aquafina");
+
+            int pepsiCount = 6 - cokeCount; // Since there are 6 questions, the remaining choices are for Pepsi
+
+            cokeCounts[i] = cokeCount;
+            pepsiCounts[i] = pepsiCount;
+        }
 
         System.out.println("Survey Results:");
-        System.out.println("Coke products selected: " + cokeCount);
-        System.out.println("Pepsi products selected: " + pepsiCount);
+        for (int i = 0; i < numberOfUsers; i++) {
+            System.out.println("User " + (i + 1) + ":");
+            System.out.println("Coke products selected: " + cokeCounts[i]);
+            System.out.println("Pepsi products selected: " + pepsiCounts[i]);
 
-        if (cokeCount > pepsiCount) {
-            System.out.println("You prefer Coke products.");
-        } else if (pepsiCount > cokeCount) {
-            System.out.println("You prefer Pepsi products.");
-        } else {
-            System.out.println("You have no clear preference between Coke and Pepsi products.");
+            if (cokeCounts[i] > pepsiCounts[i]) {
+                System.out.println("You prefer Coke products.");
+            } else if (pepsiCounts[i] > cokeCounts[i]) {
+                System.out.println("You prefer Pepsi products.");
+            } else {
+                System.out.println("You have no clear preference between Coke and Pepsi products.");
+            }
         }
 
         scanner.close();
